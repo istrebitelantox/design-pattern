483b46e5cf5266de3e258f4f9767aad336da571e
istrebitelantox <geekpook@gmail.com>
2022-01-20 09:20:51 +0300

    Добавьте файлы проекта.

---
+++ LibDuck/Duck.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibDuck
+{
+    public abstract class Duck
+    {
+        public string Quack()
+        {
+            return "Quack" + this.GetType();
+        }
+        public string Swim()
+        {
+            return "Swim" + this.GetType();
+
+        }
+        public abstract string Display();
+    }
+}
---
+++ SolDuck/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LibDuck;
+
+namespace SolDuck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Duck[] kryak = new Duck[]
+            {
+                new MallardDuck(),
+                new RedheadDuck()
+            };
+            foreach (Duck krya in kryak)
+            {
+                Console.WriteLine(krya.Swim());
+                Console.WriteLine(krya.Quack());
+                Console.WriteLine(krya.Display());
+            }
+            Console.ReadKey();
+        }
+    }
+}
